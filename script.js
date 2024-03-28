5952fd9157be8eaffa709843839c2b6713771590
Glitch (glitch-hello-website) <none>
2024-03-28 00:32:46 +0000

    ✌️💇 Checkpoint ./style.css:66266784/2 ./script.js:59189980/329:66266784/146 ./index.html:59189980/31

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var deltatime = 0
 var lasttime = 0
 var player = {
-  points: 0
+  points: 0,
+  pointspersec: 0
 }
 lasttime = Date.now()
 setInterval(() => {
   deltatime = (Date.now() - lasttime)/1000
   lasttime = Date.now()
   document.getElementById("deltatime").innerHTML = Math.round(1/deltatime)
-  player.points += deltatime * 5
-  document.getElementById("points").innerHTML = Math.round(player.points)
+  player.points += deltatime * player.pointspersec
+  player.pointspersec += deltatime * 1
+  document.getElementById("points").innerHTML = player.points.toFixed(2)
 },1000/100)
