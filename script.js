92a038e598cc073454ba063d826336d7bcb3af3a
Glitch (glitch-hello-website) <none>
2024-03-28 23:43:51 +0000

    🍅🍼 Checkpoint ./script.js:59189980/187 ./index.html:59189980/52

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ var deltatime = 0
 var lasttime = 0
 var player = {
   points: 0,
-  pointspersec: 1,
-  goofypills: 0,
+  pointspersec: 0,
+  goofypills: 1,
   longgoofypills: 0,
   susgoofygp: 0,
   silliness: 0
 }
-function BuyGoof(x){
-  let gpprice = (player.goofypills+15)**1.5
-  if (x == 1){
-    if (player.points >= gpprice){
-      player.points -= gpprice
+function BuyGoof(x,a){
+  if (x == 1 || a){
+    var price = (player.goofypills**1.5)*15
+    if (player.points >= price){
+      player.points -= price
       player.goofypills+=1
     }
   }
@@ -26,7 +26,7 @@ setInterval(() => {
   player.points += deltatime * (player.pointspersec + player.goofypills)
   player.pointspersec += deltatime * ((((1.0001**((Math.log10(player.points < 1 ? 1 : player.points))-1))/25) * 0))
   document.getElementById("points").innerHTML = player.points.toFixed(2)
-  document.getElementById("pps").innerHTML = player.pointspersec.toFixed(2)
+  document.getElementById("pps").innerHTML = (player.pointspersec + player.goofypills).toFixed(2)
 },1000/100)
 // am bacc
 //what you do?
