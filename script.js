acfddc1a9198a7b5629e47da35f73bce2e2b75cb
Glitch (glitch-hello-website) <none>
2024-03-28 23:33:36 +0000

    🚤🖖 Checkpoint ./script.js:59189980/572

--- script.js
+++ script.js
@@ -2,15 +2,29 @@ var deltatime = 0
 var lasttime = 0
 var player = {
   points: 0,
-  pointspersec: 1
+  pointspersec: 1,
+  goofypills: 0,
+  longgoofypills: 0,
+  susgoofygp: 0,
+  silliness: 0
 }
+function BuyGoof(x){
+  let gpprice = (player.goofypills+15)**1.5
+  if (x == 1){
+    if (player.points >= gpprice){
+      player.points -= gpprice
+      player.goofypills+=1
+    }
+  }
+}
+
 lasttime = Date.now()
 setInterval(() => {
   deltatime = (Date.now() - lasttime)/1000
   lasttime = Date.now()
   document.getElementById("deltatime").innerHTML = Math.round(1/deltatime)
-  player.points += deltatime * player.pointspersec
-  player.pointspersec += deltatime * ((((1.0001**((Math.log10(player.points < 1 ? 1 : player.points))-1))/25) * 0)+ 0)
+  player.points += deltatime * (player.pointspersec + player.goofypills)
+  player.pointspersec += deltatime * ((((1.0001**((Math.log10(player.points < 1 ? 1 : player.points))-1))/25) * 0))
   document.getElementById("points").innerHTML = player.points.toFixed(2)
   document.getElementById("pps").innerHTML = player.pointspersec.toFixed(2)
 },1000/100)
