78bd22d88cf70e3fff0aa9bc223fd52e5235e842
Glitch (glitch-hello-website) <none>
2024-03-29 00:09:39 +0000

    🏀🍁 Checkpoint ./script.js:59189980/104

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var player = {
 }
 function BuyGoof(x,a){
   if (x == 1 || a){
-    var price = ((player.goofypills**1.25)*15).toFixed(2)
+    var price = ((player.goofypills**1.25)*15).toFixed(1)
     document.getElementById("gpcost").innerHTML = price
     if (player.points >= price && !a){
       player.points -= price
@@ -26,8 +26,9 @@ setInterval(() => {
   document.getElementById("deltatime").innerHTML = Math.round(1/deltatime)
   player.points += deltatime * (player.pointspersec + player.goofypills)
   player.pointspersec += deltatime * ((((1.0001**((Math.log10(player.points < 1 ? 1 : player.points))-1))/25) * 0))
+  let pntsps = (player.pointspersec + player.goofypills)
   document.getElementById("points").innerHTML = player.points.toFixed(2)
-  document.getElementById("pps").innerHTML = (player.pointspersec + player.goofypills).toFixed(2)
+  document.getElementById("pps").innerHTML = pntsps.toFixed(2)
   BuyGoof(1,true)
 },1000/100)
 // am bacc
